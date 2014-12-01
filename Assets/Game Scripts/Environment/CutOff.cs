5339783d6937b9a1d0d728bf70515f64b90c94c7
unknown <s115414@S115414.campus.tue.nl>
2014-12-01 15:16:03 +0100

    First test of game engine

---
+++ Assets/Game Scripts/Environment/CutOff.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutOff : MonoBehaviour {
+
+	public GameObject player;
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update(){
+		if(player.transform.position.x < transform.position.x){
+			player.transform.position = new Vector3(
+				transform.position.x,
+				player.transform.position.y,
+				player.transform.position.z
+			);
+		}
+	}
+}
---
+++ Assets/Game Scripts/Environment/SpawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+	
+	public GameObject player;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		spawnPlayer ();
+	}
+
+	void spawnPlayer () {
+		player.transform.position = transform.position;
+		this.enabled = false;
+	}
+}
