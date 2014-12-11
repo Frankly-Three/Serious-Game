437d20d89e04feed93122b5a905ea8e5ffb6287b
Frank van de Haterd <fvdhaterd@gmail.com>
2014-12-11 16:15:09 +0100

    Basic UI panel and button, with On Click event attached to a custom made script.

---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public GameObject player;
+
+	public void ButtonClickTest() {
+		player.transform.localScale += new Vector3(2f, 2f, 0);
+	}
+
+	
+}
