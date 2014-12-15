14a662548c39d952202c011c1f9bac8f120aebe7
skiwi <frank_skiwi@hotmail.com>
2014-12-15 21:36:36 +0100

    Added basic physics for the player.

---
+++ Assets/Game Scripts/Engine/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float sidewaysModifier;
+	public float jumpingModifier;
+	public float raycastMargin;
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (!IsGrounded()) {
+			return;
+		}
+		//can only move left/right and jump when on the ground
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			gameObject.rigidbody.AddForce(Vector3.right * sidewaysModifier);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			gameObject.rigidbody.AddForce(Vector3.left * sidewaysModifier);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			gameObject.rigidbody.AddForce(Vector3.up * jumpingModifier);
+		}
+	}
+
+	bool IsGrounded() {
+		return Physics.Raycast(gameObject.transform.position, -Vector3.up, collider.bounds.extents.y + raycastMargin);
+	}
+}
