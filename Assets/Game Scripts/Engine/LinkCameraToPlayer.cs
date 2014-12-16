516a31050c9d688775d6ce6747baf35c2b6c2a20
skiwi <frank_skiwi@hotmail.com>
2014-12-16 11:05:19 +0100

    Linked camera movement again to the player.

---
+++ Assets/Game Scripts/Engine/LinkCameraToPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkCameraToPlayer : MonoBehaviour {
+	public bool linkX;
+	public bool linkY;
+	public bool linkZ;
+	
+	private GameObject player;
+	
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = (linkX) ? player.transform.position.x : gameObject.transform.position.x;
+		float y = (linkY) ? player.transform.position.y : gameObject.transform.position.y;
+		float z = (linkZ) ? player.transform.position.z : gameObject.transform.position.z;
+		gameObject.camera.transform.position = new Vector3(x, y, z);
+	}
+}
