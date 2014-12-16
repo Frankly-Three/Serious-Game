1dcecb0929fa5e4f59af6349ca30d988469fd1f2
skiwi <frank_skiwi@hotmail.com>
2014-12-16 13:13:00 +0100

    Push buttons work regardless of direction now.

---
+++ Assets/Game Scripts/Petrinets/ConstantForceEqualtToGravity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantForceEqualtToGravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+
+	void FixedUpdate() {
+		gameObject.rigidbody.AddRelativeForce(Physics.gravity * gameObject.rigidbody.mass);
+	}
+}
