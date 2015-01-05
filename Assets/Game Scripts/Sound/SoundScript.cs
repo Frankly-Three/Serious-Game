34350650ba79659fa80765131d15b573c84c0ca1
skiwi <frank_skiwi@hotmail.com>
2015-01-05 14:58:37 +0100

    Added walking sound.

--- Assets/Game Scripts/Sound/SoundScript.cs
+++ Assets/Game Scripts/Sound/SoundScript.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class SoundScript : MonoBehaviour {
 	public float raycastMargin = 0.1f;
+	public float walkingVelocityMargin = 0.05f;
 
 	public AudioClip jumpSound;
 	public AudioClip touchDownSound;
+	public AudioClip walkSound;
 
 	private GameObject player;
 	private bool isPlayerJumping;
+
+	private Vector3 lastPlayerPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +21,12 @@ public class SoundScript : MonoBehaviour {
 
 	void Awake() {
 		player = GameObject.FindGameObjectWithTag("Player");
+		lastPlayerPosition = player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//jumping and touchdown sounds
 		if (isPlayerJumping && IsPlayerGrounded()) {
 			audio.PlayOneShot(touchDownSound);
 			isPlayerJumping = false;
@@ -32,6 +38,20 @@ public class SoundScript : MonoBehaviour {
 		if (!IsPlayerGrounded()) {
 			isPlayerJumping = true;
 		}
+
+		//walk sounds
+		Vector3 deltaVelocity = (player.transform.position - lastPlayerPosition) / Time.deltaTime;;
+		if (Mathf.Abs(deltaVelocity.x) >= walkingVelocityMargin && IsPlayerGrounded()) {
+			if (audio.clip == null || !audio.clip.Equals(walkSound)) {
+				audio.clip = walkSound;
+				audio.loop = true;
+				audio.Play();
+			}
+		} else {
+			audio.clip = null;
+			audio.loop = false;
+		}
+		lastPlayerPosition = player.transform.position;
 	}
 
 	private bool IsPlayerGrounded() {
