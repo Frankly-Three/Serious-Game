3fa1b5e417a69adb4ea385d7a0941988e3a6ecab
skiwi <frank_skiwi@hotmail.com>
2015-01-11 16:19:23 +0100

    Added fancy particle effects to the mouse.

---
+++ Assets/FollowMouseInWorldScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouseInWorldScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.z = 10f;
+		transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+		Debug.Log(transform.position);
+	}
+}
