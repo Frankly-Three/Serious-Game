bda7e19b70529082e540d8ff686dba11f68c2cca
skiwi <frank_skiwi@hotmail.com>
2015-01-11 15:20:45 +0100

    Added initial menu.

---
+++ Assets/GUIScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+	public GUISkin h1skin;
+	public GUISkin h2skin;
+	public GUISkin h3skin_active;
+	public GUISkin h3skin_inactive;
+
+	void OnGUI() {
+		int widthOffset = 100;
+		int heightOffset = 100;
+
+		int width = Screen.width - (2 * widthOffset);
+		int height = Screen.height - (2 * heightOffset);
+		GUI.Box(new Rect(widthOffset, heightOffset, width, height), "");
+
+		GUI.skin = h1skin;
+		GUI.Label(rectFromMiddle(325, 550, heightOffset + 100, 50), "Serious Game");
+
+		GUI.skin = h2skin;
+		GUI.Button(rectFromMiddle(400, heightOffset + 200, 50), "Play");
+		GUI.skin = h3skin_inactive;
+		GUI.Button(rectFromMiddle(350, heightOffset + 250, 35), "Level 1");
+		GUI.Button(rectFromMiddle(350, heightOffset + 285, 35), "Level 2");
+		GUI.skin = h3skin_active;
+		if (GUI.Button(rectFromMiddle(350, heightOffset + 320, 35), "Level 3")) {
+			Application.LoadLevel("MainLevel");
+		}
+
+		GUI.skin = h2skin;
+		GUI.Button(rectFromMiddle(400, heightOffset + 355, 50), "Wiki");
+		GUI.Button(rectFromMiddle(400, heightOffset + 405, 50), "Exit");
+	}
+
+	private Rect rectFromMiddle(int width, int heightOffset, int height) {
+		int offsetFromMiddle = width / 2;
+		int middleX = Screen.width / 2;
+		return new Rect(middleX - offsetFromMiddle, heightOffset, offsetFromMiddle * 2, height);
+	}
+	
+	private Rect rectFromMiddle(int width, int width2, int heightOffset, int height) {
+		int offsetFromMiddle = width / 2;
+		int middleX = Screen.width / 2;
+		return new Rect(middleX - offsetFromMiddle, heightOffset, offsetFromMiddle + (width2 / 2), height);
+	}
+}
